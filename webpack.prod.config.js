cc5994b3a3ac95ab498567e7355a8340fe910839
Libo Cannici <lcannici@zendesk.com>
2015-10-16 11:53:33 +0200

    Heroku

---
+++ webpack.prod.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: [
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    },
+    {
+      test: /\.css?$/,
+      loaders: ['style', 'raw'],
+      include: __dirname
+    }]
+  }
+};
+
+
